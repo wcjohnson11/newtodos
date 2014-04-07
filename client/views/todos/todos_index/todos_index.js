e3c829b2b1e827a217c9abc821ac9171a03afc42
wcjohnson11 <williamcouperjohnson@gmail.com>
2014-04-07 16:14:57 -0700

    Added Scaffoling

---
+++ client/views/todos/todos_index/todos_index.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* TodosIndex: Event Handlers and Helpers */
+/*****************************************************************************/
+Template.TodosIndex.events({
+  /*
+   * Example: 
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.TodosIndex.helpers({
+  /*
+   * Example: 
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* TodosIndex: Lifecycle Hooks */
+/*****************************************************************************/
+Template.TodosIndex.created = function () {
+};
+
+Template.TodosIndex.rendered = function () {
+};
+
+Template.TodosIndex.destroyed = function () {
+};
